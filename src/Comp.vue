8c62d8d0ffc29f2b22acf72b09f986e6ade70431
小的的 DeDe <wiixdede@gmail.com>
2024-03-14 20:57:49 +0800

    Initial commit

---
+++ src/Comp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <p class="v-comp-text">
+    hello <strong>{{ name }}</strong>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
